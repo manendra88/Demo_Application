91d71d5b0300c8ccb63c99fc562ec221c803f95a
SD67119 <manendra.singh@external.stellantis.com>
2024-01-05 11:40:08 +0530

    initial commit

--- src/main/java/com/mycompany/demoapplication/Controller/HelloController.java
+++ src/main/java/com/mycompany/demoapplication/Controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Demo Apps!";
     }
 
 }
