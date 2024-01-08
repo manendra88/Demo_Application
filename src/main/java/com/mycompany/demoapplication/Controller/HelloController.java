46af43a6476c57bc0856cae2d7ace061b9cdf83b
manendra88 <manendra07150@gmail.com>
2024-01-08 18:17:00 +0530

    Update HelloController.java

--- src/main/java/com/mycompany/demoapplication/Controller/HelloController.java
+++ src/main/java/com/mycompany/demoapplication/Controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String index() {
-        return "Greetings from Demo Apps from helm chart ";
+        return "Greetings from Demo Apps helm redhat ";
     }
 
 }
